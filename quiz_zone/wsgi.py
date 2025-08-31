0ca1051f8bf6928400f55ae9e2cd1388a650b990
Rafsunjany-khan <107175695+Rafsunjany-khan@users.noreply.github.com>
2025-09-01 02:43:45 +0600

    Update wsgi.py file for PythonAnywhere run project

--- quiz_zone/wsgi.py
+++ quiz_zone/wsgi.py
@@ -1,13 +1,19 @@
 import sys
 import os
 
-# add your project folder to the sys.path
+# Add project folder to sys.path
 path = '/home/rafsunjany/Quiz-Zone'
 if path not in sys.path:
     sys.path.append(path)
 
-# set the settings module
+# Activate virtualenv
+activate_this = '/home/rafsunjany/Quiz-Zone/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz_zone.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
